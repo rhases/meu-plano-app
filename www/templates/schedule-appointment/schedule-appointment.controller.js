d9f9736de126ad2f1814757b7f0db26e0a3fc965
Diego Marcilio <diego.marcilio@rhases.com.br>
2016-06-29 10:29:40 -0300

    Iterando especialidades da variável definida no escopo via controller.
    - Posteriormente essa variável deve ser obtida atráves de um serviço.

--- www/templates/schedule-appointment/schedule-appointment.controller.js
+++ www/templates/schedule-appointment/schedule-appointment.controller.js
@@ -1,5 +1,7 @@
 // Controller of defaultUserInterface.
 appControllers.controller('scheduleAppointmentCtrl', function ($scope, $mdBottomSheet, $mdToast, $mdDialog) {
 
+        // FIXME: service para pegar do banco as especialidades
+        $scope.specialties = ['Cardiologista', 'Dermatologista', 'Urologista', 'Oftamologista'];
 
 }); // End of defaultUserInterface controller.
