2a5a267a055b33b33ee18ba7b4fa7ff56c41ab3a
Tales Porto <talesap@gmail.com>
2016-07-01 16:24:56 -0300

    update das libs usadas por nós!

--- www/js/module.js
+++ www/js/module.js
@@ -9,3 +9,6 @@ angular.module('starter',
 		'ngCordova'
 	]
 )
+
+var appControllers = angular.module('starter.controllers', []); // Use for all controller of application.
+var appServices = angular.module('starter.services', []);// Use for all service of application.
