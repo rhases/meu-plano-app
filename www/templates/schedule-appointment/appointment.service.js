62fe5f53117c3faaa16dab708fb04654b1b95066
Diego Marcilio <diego.marcilio@rhases.com.br>
2016-07-04 16:57:02 -0300

    Checkboxes para escolha de período.
    - Períodos iguais ao do formulário.

--- www/templates/schedule-appointment/appointment.service.js
+++ www/templates/schedule-appointment/appointment.service.js
@@ -11,7 +11,15 @@ appServices.factory('appointmentService', function (specialtiesService) {
                     {label: 'sexta-feira', checked: false},
                     {label: 'sábado', checked: false},
                     {label: 'domingo', checked: false}
-                ]
+                ],
+            periods: [
+                {label: 'Início da manhã (7h-10h)', checked: false},
+                {label: 'Fim da manhã (10h-12h)', checked: false},
+                {label: 'Horário do almoço (12h-14h)', checked: false},
+                {label: 'Início da tarde (14h-16h)', checked: false},
+                {label: 'Fim da tarde (16h-18h)', checked: false},
+                {label: 'Início da noite (18h-20h)', checked: false}
+            ]
         }
 
     }
