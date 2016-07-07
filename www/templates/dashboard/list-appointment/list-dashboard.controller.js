3b76bd4e159fdff1fc13fbd036d62614b352b9b4
AMAndrioli <andriolidevelop@gmail.com>
2016-07-07 17:56:22 -0300

    implementação das funcionalidades da tela de agendamento

--- www/templates/dashboard/list-appointment/list-dashboard.controller.js
+++ www/templates/dashboard/list-appointment/list-dashboard.controller.js
@@ -1,5 +1,5 @@
 // Controller of dashboard.
-appControllers.controller('dashboardListCtrl', function ($scope, $timeout, $state, $stateParams, $ionicHistory, lodash, $mdDialog, appointmentService, APPOINTMENT_STATUS) {
+appControllers.controller('listAppointmentController', function ($scope, $timeout, $state, $stateParams, $ionicHistory, lodash, $mdDialog, appointmentService, APPOINTMENT_STATUS) {
 
     //$scope.isAnimated is the variable that use for receive object data from state params.
     //For enable/disable row animation.
@@ -19,7 +19,7 @@ appControllers.controller('dashboardListCtrl', function ($scope, $timeout, $stat
     // by using targetPage to be the destination state.
     // Parameter :
     // stateNames = target state to go.
-    $scope.navigateTo = function (stateName) {
+    $scope.navigateTo = function(stateName) {
         $timeout(function () {
             if ($ionicHistory.currentStateName() != stateName) {
                 $ionicHistory.nextViewOptions({
@@ -32,13 +32,41 @@ appControllers.controller('dashboardListCtrl', function ($scope, $timeout, $stat
     }; // End of navigateTo.
 
     // goToSetting is for navigate to Dashboard Setting page
-    $scope.goToSetting = function () {
+    $scope.goToSetting = function() {
         $state.go("app.setting");
     };// End goToSetting.
 
     // cancel appointment
-    $scope.cancelAppointment = function ($event) {
+    $scope.cancelAppointment = function($event) {
     }// End cancel appointment.
+
+    $scope.relativeDateFormat = function(data) {
+        return moment(String(data)).locale('pt-BR').fromNow();
+    }
+
+    $scope.formatDate = function(data) {
+        return moment(String(data)).locale("pt-BR").format("DD/MM/YY [as] h:mm");
+    }
+
+    $scope.cancelScheduledAppointment = function(appointmentId) {
+        var appointment = $scope.scheduledAppointment.filter(function(appointment) {
+            return appointmentId === appointment._id;
+        });
+
+        $state.go("app.dashboard-detail", {
+            "appointment": appointment
+        });
+    }
+
+    $scope.cancelAcceptedAppointment = function(appointmentId) {
+        var appointment = $scope.acceptedAppointment.filter(function(appointment) {
+            return appointmentId === appointment._id;
+        });
+
+        $state.go("app.dashboard-detail", {
+            "appointment": appointment
+        });
+    }
 
     function divideByStatus(listAppointment) {
         var scheduledAppointment = listAppointment.filter(function(appointment) {
