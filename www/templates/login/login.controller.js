c5e59b3d0f4390e34231b8fa19fb9c1a98392ec8
Marcos Godinho <mvsgodinho@gmail.com>
2016-07-08 17:42:19 -0300

    Consulta agendada no dashboard (incompleto)

--- www/templates/login/login.controller.js
+++ www/templates/login/login.controller.js
@@ -72,8 +72,8 @@ appControllers.controller('loginCtrl', function($scope, $state, $q, $ionicLoadin
 			template: 'Login de teste...'
 		});
 
-		// profileInfo = {email:"mvsgodinho@gmail.com",name:"Marcos Vinícius Silva Godinho", _id:"1"};
-		profileInfo = {email:"talesap@gmail.com", name:"Tales Porto", _id:"1"};
+		profileInfo = {email:"mvsgodinho@gmail.com",name:"Marcos Vinícius Silva Godinho", _id:"1"};
+		//profileInfo = {email:"talesap@gmail.com", name:"Tales Porto", _id:"1"};
 		authService.facebookSignUp(profileInfo)
 			.then(function(user) {
 				rhasesLoginSuccess(user);
