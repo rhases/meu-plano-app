74a0cc2c3035d21c063bf389069b0bf09c1cd218
AMAndrioli <andriolidevelop@gmail.com>
2016-07-11 14:05:56 -0300

    Tirando funções inuteis

--- www/templates/dashboard/detail-appointment/detail-appointment.controller.js
+++ www/templates/dashboard/detail-appointment/detail-appointment.controller.js
@@ -8,21 +8,4 @@ appControllers.controller("detailAppointmentController", function($scope, $state
 		}
     }
 
-    $scope.getMedicalSpecialtyLabel = function(specialityP) {
-        var speciality = medicalInfos.getByCod(String(specialityP));
-		if (!speciality)
-	        return;
-
-        return speciality.label;
-    }
-
-    $scope.getCityLabel = function(cityCod) {
-        var city = brazilianInfos.getCityByCod("df", String(cityCod));
-
-        if (!city)
-            return;
-
-        return city.label;
-    }
-
 });
