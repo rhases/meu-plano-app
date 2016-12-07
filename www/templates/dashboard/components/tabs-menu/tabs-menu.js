98df2ab691700a271af88308e0c4d72c884b9fb0
AMAndrioli <andriolidevelop@gmail.com>
2016-12-07 14:08:34 -0200

    tabs menu

---
+++ www/templates/dashboard/components/tabs-menu/tabs-menu.js
@@ -0,0 +1,12 @@
+appControllers.directive('tabsMenu', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/dashboard/components/tabs-menu/tabs-menu.html',
+            scope: {
+                "state": "="
+            },
+            controller: function() {
+
+            }
+        };
+    });
