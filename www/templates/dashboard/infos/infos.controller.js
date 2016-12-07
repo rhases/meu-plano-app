7fcd5831750b9ef925c462247361c510b6352652
Diego Marcilio <diego.marcilio@rhases.com.br>
2016-12-07 14:38:02 -0200

    Mostrando toscamente os dados do plano.

--- www/templates/dashboard/infos/infos.controller.js
+++ www/templates/dashboard/infos/infos.controller.js
@@ -1,5 +1,9 @@
 appControllers.controller('infosCtrl', function ($http, $scope, $rootScope, $timeout, $state, $stateParams, $q, lodash) {
 
+        $scope.userProfile = {
+            "age": 35
+        }
+
         $scope.healthPlan = {
             "_id": 471802140,
             "name": "Bradesco Saúde Top Nacional 2 E CA copart",
@@ -34,4 +38,43 @@ appControllers.controller('infosCtrl', function ($http, $scope, $rootScope, $tim
             return lodash.join(coverageTypes, ', ');
         }
 
+        $scope.maxPrice = function() {
+            var maxPriceUnformatted = retrieveMaxPrice();
+            var maxPriceTwoDecimal = maxPriceUnformatted.toFixed(2);
+            return maxPriceTwoDecimal.toLocaleString();
+        }
+
+        function retrieveMaxPrice() {
+            var maxPrice = $scope.healthPlan.maxPrice;
+            var userAge = $scope.userProfile.age;
+            var ageRange = getAgeRange(userAge);
+            return maxPrice[ageRange];
+        }
+
+        var FULL_LIFE_RANGE_KEYS =
+        	['a18orLess', 'a19to23', 'a24to28', 'a29to33', 'a34to38', 'a39to43', 'a44to48', 'a49to53', 'a54to58', 'a59orMore'];
+
+        function getAgeRange(age) {
+    		if (age <= 18) {
+    			return FULL_LIFE_RANGE_KEYS[0];
+    		} else if ((age > 18) && (age <= 23)) {
+    			return FULL_LIFE_RANGE_KEYS[1];
+    		} else if ((age > 23) && (age <= 28)) {
+    			return FULL_LIFE_RANGE_KEYS[2];
+    		} else if ((age > 28) && (age <= 33)) {
+    			return FULL_LIFE_RANGE_KEYS[3];
+    		} else if ((age > 33) && (age <= 38)) {
+    			return FULL_LIFE_RANGE_KEYS[4];
+    		} else if ((age > 38) && (age <= 43)) {
+    			return FULL_LIFE_RANGE_KEYS[5];
+    		} else if ((age > 43) && (age <= 48)) {
+    			return FULL_LIFE_RANGE_KEYS[6];
+    		} else if ((age > 48) && (age <= 53)) {
+    			return FULL_LIFE_RANGE_KEYS[7];
+    		} else if ((age > 53) && (age <= 58)) {
+    			return FULL_LIFE_RANGE_KEYS[8];
+    		} else if (age > 58) {
+    			return FULL_LIFE_RANGE_KEYS[9];
+    		}
+    	}
 });
