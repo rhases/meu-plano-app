49d7637238762b7ff7ec0c402283469980866206
AMAndrioli <andriolidevelop@gmail.com>
2016-12-08 15:38:39 -0200

    geolocation funfando :D

--- www/templates/dashboard/emergency/emergency.controller.js
+++ www/templates/dashboard/emergency/emergency.controller.js
@@ -10,7 +10,15 @@ appControllers.controller('emergencyHospitalsController', function($http, $scope
             return $q.when()
                 .then(getCurrentPosition())
                 .then(createMap())
-                .then(populateHospitals(hospitals));
+                .then(populateHospitals(hospitals))
+                .catch(function(error) {
+                    console.log('emergencyHospitalsController ');
+                    console.log(error.message);
+                });
+        })
+        .catch(function(error) {
+            console.log('emergencyHospitalsController ');
+            console.log(error.message);
         })
 
 	$scope.doRefresh = function() {
@@ -96,7 +104,6 @@ appControllers.controller('emergencyHospitalsController', function($http, $scope
 	}
 
 	function getHospitals() {
-
         return Providers.getHospitals({'state': 'df', 'city': 'brasilia', 'plan': 471802140}).$promise
                 .then(function(hospitals) {
                     $scope.emergencyHospitals = hospitals;
