ce7ee56c7abe04c8d7ef95737d576635e02bfb57
Diego Marcilio <diego.marcilio@rhases.com.br>
2016-12-09 18:56:36 -0200

    Complementando/melhorando informações do plano.

--- www/js/utils/popover-text.service.js
+++ www/js/utils/popover-text.service.js
@@ -3,11 +3,11 @@ angular.module('starter').factory('popoverText', function(lodash) {
 
     service.coverageTypeText = function(coverageType) {
         if(coverageType === 'ambulatorial')
-            return 'Consultas médicas em clínicas ou consultórios, exames, tratamentos e demais procedimentos ambulatoriais. Os atendimentos de emergência estão limitados até as primeiras 12 horas do atendimento. <br /> <br /> Não cobre internação.';
+            return 'Consultas médicas em clínicas ou consultórios, exames, tratamentos e demais procedimentos ambulatoriais. Os atendimentos de emergência estão limitados até as primeiras 12 horas do atendimento.';
         if(coverageType === 'hospitalar')
             return 'Internação hospitalar sem limite de tempo. <br /> <br /> Não cobre partos.';
         if(coverageType === 'obstetricia')
-            return 'Cobertura assistencial ao recém-nascido filho natural ou adotivo, ou do dependente, durante os primeiros 30 dias após o parto.';
+            return 'Parto e cobertura assistencial ao recém-nascido filho natural ou adotivo, ou do dependente, durante os primeiros 30 dias após o parto.';
         if(coverageType === 'odontologia')
             return 'Consultas, exames, atendimentos de urgência e emergência odontológicos, exames auxiliares ou complementares, tratamentos e demais procedimentos.';
         return '';
