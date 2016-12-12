ed8394bc2e92ae1b187aabec9d267fdef7e90e10
AMAndrioli <andriolidevelop@gmail.com>
2016-12-12 16:23:09 -0200

    removendo realmento o $rootScope.appUser

--- www/templates/login/login.controller.js
+++ www/templates/login/login.controller.js
@@ -1,4 +1,3 @@
 appControllers.controller('loginCtrl', function($scope, authService, $rootScope) {
-    if (!$rootScope.appUser || !$rootScope.appUser.healthPlan)
-        authService.logout();
+    
 });
