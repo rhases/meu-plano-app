a3b8eed4a18d728cad0c30c313e85b5b318781f5
Tales Porto <talesap@gmail.com>
2016-12-12 14:45:13 -0200

    Removendo lixo.

--- www/js/app.constants.js
+++ www/js/app.constants.js
@@ -1,24 +1,6 @@
 angular.module("starter")
-	.constant("ANS_WS_URI", "http://answs.herokuapp.com/api/")
-	// .constant("ANS_WS_URI", "http://localhost:9002/")
-    .constant("AUTH_SERVER_URI", "http://auth.api.rhases.com.br/")
+	// .constant("ANS_WS_URI", "http://answs.herokuapp.com/api/")
+	.constant("ANS_WS_URI", "http://localhost:9002/api/")
 
-	.constant("GCM_SENDER_ID", "835746108347")
-
-    .constant("APPOINTMENT_STATUS", {
-        SCHEDULED: "SCHEDULED",
-        ACCEPTED: "ACCEPTED",
-        REFUSED: "REFUSED",
-        CONFIRMED: "CONFIRMED",
-        CANCELED: "CANCELED",
-        RESCHEDULED: "RESCHEDULED"
-    })
-
-    .constant("APPOINTMENT_REQUEST_STATUS", {
-        NEW: "NEW",
-        SCHEDULED: "SCHEDULED",
-        CANCELED: "CANCELED",
-        NOT_POSSIBLE: "NOT_POSSIBLE"
-    })
-
-    .constant('GA_TRACKING_ID', 'UA-51103102-3');
+	.constant("GCM_SENDER_ID", "XXX")
+    .constant('GA_TRACKING_ID', 'XXX')
